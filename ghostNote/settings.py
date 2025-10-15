d90e558337dc608b8a0028296fb3086819644794
N Sarif <24-CSE-145nur@eitfaridabad.co.in>
2025-10-15 17:46:30 +0530

    added whitenoise settings

--- ghostNote/settings.py
+++ ghostNote/settings.py
@@ -47,6 +47,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # -------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -108,6 +109,8 @@ USE_TZ = True
 # -------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # -----------------------
 # Default primary key field type
