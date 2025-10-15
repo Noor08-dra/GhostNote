511cf5476e25fa12c3a6223f34e88604bbb6edb9
N Sarif <24-CSE-145nur@eitfaridabad.co.in>
2025-10-15 16:24:54 +0530

    new version

--- ghostNote/settings.py
+++ ghostNote/settings.py
@@ -80,13 +80,11 @@ WSGI_APPLICATION = 'ghostNote.wsgi.application'
 # -------------------------
 # Local: SQLite, Production: Render/Postgres
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 # -------------------------
 # Password validation
 # -------------------------
